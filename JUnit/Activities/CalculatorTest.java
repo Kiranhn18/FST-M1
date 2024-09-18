3e431ecb39b33afecb727ebe4f8f67a36988a1ad
Kiranhn18 <Kiran.Nageshwar@ibm.com>
2024-09-18 12:05:23 +0530

    Add files via upload

---
+++ JUnit/Activities/CalculatorTest.java
@@ -0,0 +1,29 @@
+package demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+// The Test class should start or end with "Test"
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("Simple multiplication should work")
+    public void testMultiply() {
+        assertEquals(20, calculator.multiply(4, 5));
+        assertNotEquals(3, calculator.add(1, 2)); // Fails
+        
+    }
+}	    
+
