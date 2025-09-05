4569f5c9796f8e7d4d88e0366d92dc3c60662f9a
Kiranhn18 <Kiran.Nageshwar@ibm.com>
2025-09-05 19:18:30 +0530

    Add files via upload

---
+++ Appium/Activities/Activity1.java
@@ -0,0 +1,63 @@
+package activities;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+public class Activity1 {
+
+
+	    // Driver Declaration
+	    AndroidDriver driver;
+
+	    // Set up method
+	    @BeforeClass
+	    public void setUp() throws MalformedURLException, URISyntaxException {
+	        // Desired Capabilities
+	        UiAutomator2Options options = new UiAutomator2Options();
+	        options.setPlatformName("android");
+	        options.setAutomationName("UiAutomator2");
+	        //options.setApp("C:/Users/KiranNageshwar/Downloads/Calculator2.apk/");
+	        options.setAppPackage("com.google.android.calculator");
+	        options.setAppActivity("com.android.calculator2.Calculator");
+	        options.noReset();
+
+	        // Server Address
+	        URL serverURL = new URI("http://localhost:4723").toURL();
+
+	        // Driver Initialization
+	        driver = new AndroidDriver(serverURL, options);
+	    }
+
+	    // Test method
+	    @Test
+	    public void multiplyTest() {
+	        // Perform the calculation
+	        driver.findElement(AppiumBy.id("digit_5")).click();
+	        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+	        driver.findElement(AppiumBy.id("digit_9")).click();
+	        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+	        // Find the result
+	        String result = driver.findElement(AppiumBy.id("result_final")).getText();
+
+	        // Assertion
+	        Assert.assertEquals(result, "45");
+	    }
+
+
+	    // Tear down method
+	    @AfterClass
+	    public void tearDown() {
+	        // Close the app
+	        driver.quit();
+	    }
+	}
+
